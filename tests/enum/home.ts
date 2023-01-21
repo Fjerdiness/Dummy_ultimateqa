9297a345f764abd8001080c5a8d1bf49033bd9a5
Fjerdiness <vocable_courts.05@icloud.com>
2023-01-21 20:41:23 +0000

    first dummy test

--- tests/enum/home.ts
+++ tests/enum/home.ts
@@ -1,3 +1,7 @@
 export enum HomePageSelector {
-    title = '',
+    titleSelector = '.section__heading section__heading-ornament section__heading___d6c74',
+    titleText = 'Ultimate QA',
+    subTitleSelector = '.section__subheading section__subheading___d6c74',
+    subTitleText = 'Worldclass Automation Training',
+
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,16 +1,8 @@
 import { test, expect } from '@playwright/test';
+import { HomePageSelector } from './enum/home';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(page.locator(HomePageSelector.titleSelector)).toHaveText(HomePageSelector.titleText);
+  await expect(page.locator(HomePageSelector.subTitleSelector)).toHaveText(HomePageSelector.subTitleText)
 });
